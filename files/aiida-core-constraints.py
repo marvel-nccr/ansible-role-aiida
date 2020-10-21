a1753185ce423c5bcf7abe8680996014259d693e
Chris Sewell <chrisj_sewell@hotmail.com>
2020-10-21 05:31:18 +0200

    Add generation/use of pip constraints file

---
+++ files/aiida-core-constraints.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Script to convert aiida setup.json to pip constraints file.
+
+Usage::
+
+    python aiida-core-constraints.py path/to/setup.json extras1 extras2 ...
+"""
+import json
+from pathlib import Path
+import sys
+
+def main(path, *extras):
+    path = Path(path)
+    data = json.loads(path.read_text("utf8"))
+
+    output = ["# aiida-core requirements"]
+    output.append("aiida-core==" + data["version"])
+    output.extend(data["install_requires"])
+
+    for extra in extras:
+        output.append("# " + extra)
+        output.extend(data["extras_require"][extra])
+
+    print("\n".join(output))
+
+if __name__ == "__main__":
+    assert len(sys.argv) > 1, "JSON path required"
+    main(sys.argv[1], *sys.argv[2:])
